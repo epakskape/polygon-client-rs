d7335c30ce8bc8662234c18c375a9b291b83e8c0
Matt Miller <hickmm@outlook.com>
2021-07-11 20:17:48 -0500

    progress so far

---
+++ src/client.rs
@@ -0,0 +1,168 @@
+use std::env;
+use std::fmt;
+use std::collections::HashMap;
+
+use crate::types::*;
+
+static DEFAULT_API_URL: &str = "https://api.polygon.io";
+
+pub struct Client {
+    pub auth_key: String,
+    pub api_url: String,
+    pub timeout: Option<u32>,
+    client: reqwest::Client,
+}
+
+impl Client {
+    pub fn new(auth_key: Option<&str>, timeout: Option<u32>) -> Self {
+        let api_url = match env::var("POLYGON_API_URL") {
+            Ok(v) => v,
+            _ => String::from(DEFAULT_API_URL),
+        };
+
+        let auth_key_actual = match auth_key {
+            Some(v) => String::from(v),
+            _ => match env::var("POLYGON_AUTH_KEY") {
+                Ok(v ) => String::from(v),
+                _ => panic!("POLYGON_AUTH_KEY not set")
+            }
+        };
+
+        Client {
+            auth_key: auth_key_actual,
+            api_url: api_url,
+            timeout: timeout,
+            client: reqwest::Client::new(),
+        }
+    }
+
+    async fn send_request<RespType>(self, uri: &str, query_params: &HashMap<&str, &str>) -> Result<RespType, reqwest::Error> 
+        where RespType : serde::de::DeserializeOwned
+    {
+        let res = self.client
+            .get(format!("{}{}", self.api_url, uri))
+            .bearer_auth(self.auth_key)
+            .query(query_params)
+            .send()
+            .await?;
+
+        res.json::<RespType>().await
+    }
+
+    pub async fn reference_tickers(self, query_params: &HashMap<&str, &str>) -> Result<ReferenceTickersResponse, reqwest::Error> {
+        self.send_request::<ReferenceTickersResponse>("/v3/reference/tickers", query_params).await
+    }
+
+    pub async fn reference_ticker_types(self, query_params: &HashMap<&str, &str>) -> Result<ReferenceTickerTypesResponse, reqwest::Error> {
+        self.send_request::<ReferenceTickerTypesResponse>("/v2/reference/types", query_params).await
+    }
+    
+    pub async fn reference_ticker_details(self, stocks_ticker: &str, query_params: &HashMap<&str, &str>) -> Result<ReferenceTickerDetailsResponse, reqwest::Error> {
+        let uri = format!("/v1/meta/symbols/{}/company", stocks_ticker);
+        self.send_request::<ReferenceTickerDetailsResponse>( &uri, query_params).await
+    }
+    
+    pub async fn reference_ticker_details_vx(self, stocks_ticker: &str, query_params: &HashMap<&str, &str>) -> Result<ReferenceTickerDetailsResponseVX, reqwest::Error> {
+        let uri = format!("/vX/reference/tickers/{}", stocks_ticker);
+        self.send_request::<ReferenceTickerDetailsResponseVX>( &uri, query_params).await
+    }
+    
+    pub async fn reference_ticker_news(self, query_params: &HashMap<&str, &str>) -> Result<ReferenceTickerNewsResponse, reqwest::Error> {
+        self.send_request::<ReferenceTickerNewsResponse>("/v2/reference/news", query_params).await
+    }
+    
+    pub async fn reference_markets(self, query_params: &HashMap<&str, &str>) -> Result<ReferenceMarketsResponse, reqwest::Error> {
+        self.send_request::<ReferenceMarketsResponse>("/v2/reference/markets", query_params).await
+    }
+    
+    pub async fn reference_locales(self, query_params: &HashMap<&str, &str>) -> Result<ReferenceLocalesResponse, reqwest::Error> {
+        self.send_request::<ReferenceLocalesResponse>("/v2/reference/locales", query_params).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::client::Client;
+
+    #[test]
+    fn test_reference_tickers() {
+        let mut query_params = HashMap::new();
+        query_params.insert("ticker", "MSFT");
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_tickers(&query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+        assert_eq!(resp.count, 1);
+        assert_eq!(resp.results[0].market, "stocks");
+        assert_eq!(resp.results[0].currency_name, "usd");
+    }
+    
+    #[test]
+    fn test_reference_ticker_types() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_ticker_types(&query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+        assert_eq!(resp.results.types["CS"], "Common Stock");
+        assert_eq!(resp.results.index_types["INDEX"], "Index");
+    }
+
+    #[test]
+    fn test_reference_ticker_details() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_ticker_details("MSFT", &query_params)
+        ).unwrap();
+        assert_eq!(resp.country, "usa");
+        assert_eq!(resp.name, "Microsoft Corporation");
+        assert_eq!(resp.symbol, "MSFT");
+    }
+
+    #[test]
+    fn test_reference_ticker_details_vx() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_ticker_details_vx("MSFT", &query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+        assert_eq!(resp.results.ticker, "MSFT");
+        assert_eq!(resp.results.currency_name, "usd");
+    }
+    
+    #[test]
+    fn test_reference_ticker_news() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_ticker_news(&query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+    } 
+
+    #[test]
+    fn test_reference_markets() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_markets(&query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+        let bond = resp.results.iter().find(|x| x.market == "BONDS");
+        assert_eq!(bond.is_some(), true);
+        assert_eq!(bond.unwrap().desc, "Bonds");
+    }
+
+    #[test]
+    fn test_reference_locales() {
+        let query_params = HashMap::new();
+        let resp = tokio_test::block_on(
+            Client::new(None, None).reference_locales(&query_params)
+        ).unwrap();
+        assert_eq!(resp.status, "OK");
+        let bond = resp.results.iter().find(|x| x.locale == "US");
+        assert_eq!(bond.is_some(), true);
+        assert_eq!(bond.unwrap().name, "United States of America");
+    }
+
+
+}
---
+++ src/types.rs
@@ -0,0 +1,209 @@
+use serde;
+use serde::Deserialize;
+
+use std::collections::HashMap;
+
+//
+// v3/reference/tickers
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickersResponseTickerV3 {
+    pub ticker: String,
+    pub name: String,
+    pub market: String,
+    pub locale: String,
+    pub primary_exchange: String,
+    #[serde(rename = "type")]
+    pub ticker_type: String,
+    pub active: bool,
+    pub currency_name: String,
+    pub cik: String,
+    pub composite_figi: String,
+    pub share_class_figi: String,
+    pub last_updated_utc: String,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickersResponseV3 {
+    pub results: Vec<ReferenceTickersResponseTickerV3>,
+    pub status: String,
+    pub request_id: String,
+    pub count: u32,
+    pub next_url: Option<String>
+}
+
+pub type ReferenceTickersResponse = ReferenceTickersResponseV3;
+
+//
+// v2/reference/types
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerTypesResultsV2 {
+    pub types: HashMap<String, String>,
+    #[serde(rename = "indexTypes")]
+    pub index_types: HashMap<String, String>
+}
+ 
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerTypesResponseV2 {
+    pub status: String,
+    pub results: ReferenceTickerTypesResultsV2,
+}
+
+pub type ReferenceTickerTypesResponse = ReferenceTickerTypesResponseV2;
+
+//
+// v1/meta/symbols/{stocksTicker}/company
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerDetailsResponseV1 {
+    pub logo: String,
+    pub exchange: String,
+    #[serde(rename = "exchangeSymbol")]
+    pub exchange_symbol: String,
+    #[serde(rename = "type")]
+    pub ticker_type: String,
+    pub name: String,
+    pub symbol: String,
+    pub listdate: String,
+    pub cik: String,
+    pub bloomberg: String,
+    pub fiji: Option<String>,
+    pub sic: u32,
+    pub country: String,
+    pub industry: String,
+    pub sector: String,
+    pub marketcap: u64,
+    pub employees: u64,
+    pub phone: String,
+    pub ceo: String,
+    pub url: String,
+    pub description: String,
+    pub hq_address: String,
+    pub hq_country: String,
+    pub similar: Vec<String>,
+    pub tags: Vec<String>,
+    pub updated: String,
+    pub active: bool
+}
+
+pub type ReferenceTickerDetailsResponse = ReferenceTickerDetailsResponseV1;
+
+//
+// vX/reference/tickers/{ticker}
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Address {
+    pub address1: String,
+    pub city: String,
+    pub state: String
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerDetailsResultsVX {
+    pub ticker: String,
+    pub name: String,
+    pub market: String,
+    pub locale: String,
+    pub primary_exchange: String,
+    #[serde(rename = "type")]
+    pub ticker_type: String,
+    pub active: bool,
+    pub currency_name: String,
+    pub cik: String,
+    pub composite_fiji: Option<String>,
+    pub share_class_fiji: Option<String>,
+    pub last_updated_utc: String,
+    pub delisted_utc: Option<String>,
+    pub outstanding_shares: f64,
+    pub market_cap: f64,
+    pub phone_number: String,
+    pub address: Address,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerDetailsResponseVX {
+    pub results: ReferenceTickerDetailsResultsVX,
+    pub status: String,
+    pub request_id: String,
+    pub count: u32
+}
+
+//
+// v2/reference/news
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Publisher {
+    pub name: String,
+    pub homepage_url: String,
+    pub logo_url: String,
+    pub favicon_url: String,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerNewsResultsV2 {
+    pub id: String,
+    pub publisher: Publisher,
+    pub title: String,
+    pub author: String,
+    pub published_utc: String,
+    pub article_url: String,
+    pub tickers: Vec<String>,
+    pub amp_url: String,
+    pub image_url: String,
+    pub description: String,
+    pub keywords: Option<Vec<String>>,
+}
+
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceTickerNewsResponseV2 {
+    pub results: Vec<ReferenceTickerNewsResultsV2>,
+    pub status: String,
+    pub request_id: String,
+    pub count: u32,
+    pub next_url: Option<String>
+}
+
+pub type ReferenceTickerNewsResponse = ReferenceTickerNewsResponseV2;
+
+//
+// v2/reference/markets
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Market {
+    pub market: String,
+    pub desc: String
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceMarketsResponseV2 {
+    pub status: String,
+    pub results: Vec<Market>
+}
+
+pub type ReferenceMarketsResponse  = ReferenceMarketsResponseV2;
+
+//
+// v2/reference/locales
+//
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Locale {
+    pub locale: String,
+    pub name: String
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct ReferenceLocalesResponseV2 {
+    pub status: String,
+    pub results: Vec<Locale>,
+}
+
+pub type ReferenceLocalesResponse = ReferenceLocalesResponseV2;

873625cf92d77bd43dc8e22bd1affda59f68e937
Matt Miller <hickmm@outlook.com>
2021-08-23 21:44:02 -0500

    websocket updates

--- src/rest.rs
+++ src/rest.rs
@@ -25,7 +25,6 @@
 //!     }
 //! }
 //! ```
-//!
 use std::collections::HashMap;
 use std::env;
 
--- src/websocket.rs
+++ src/websocket.rs
@@ -1,7 +1,29 @@
-//! WebSocket client for <https://polygon.io>.
-
+//! WebSocket client for [polygon.io](https://polygon.io).
+//!
+//! # Authentication
+//!
+//! Use an [API key](https://polygon.io/dashboard/api-keys) to authenticate.
+//! This can be provided through the `auth_key` parameter to
+//! [`WebSocketClient::new()`] or through the `POLYGON_AUTH_KEY` environment variable.
+//!
+//! # Example
+//!
+//! ```
+//! use polygon_client::websocket::{STOCKS_CLUSTER, WebSocketClient};
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let mut client = WebSocketClient::new(STOCKS_CLUSTER, None);
+//!     let res = client.receive();
+//!     let msg_text = res.unwrap().into_text().unwrap();
+//!     println!("msg: {}", msg_text);
+//! }
+//! ```
 use std::env;
 use url::Url;
+
+use serde;
+use serde::Deserialize;
 
 use tungstenite::{Message, WebSocket};
 use tungstenite::client::connect;
@@ -9,6 +31,13 @@ use tungstenite::client::connect;
 pub const STOCKS_CLUSTER: &str = "stocks";
 pub const FOREX_CLUSTER: &str = "forex";
 pub const CRYPTO_CLUSTER: &str = "crypto";
+
+#[derive(Clone, Deserialize, Debug)]
+struct ConnectedMessage {
+    pub ev: String,
+    pub status: String,
+    pub message: String
+}
 
 pub struct WebSocketClient {
     pub auth_key: String,
@@ -59,10 +88,15 @@ impl WebSocketClient {
         let msg = format!("{{\"action\":\"unsubscribe\",\"params\":\"{}\"}}", &str);
         self.websocket.write_message(Message::Text(msg.into())).expect("failed to unsubscribe");
     }
+
+    pub fn receive(&mut self) -> tungstenite::error::Result<Message> {
+        self.websocket.read_message()
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::websocket::ConnectedMessage;
     use crate::websocket::WebSocketClient;
     use crate::websocket::STOCKS_CLUSTER;
 
@@ -71,5 +105,19 @@ mod tests {
         let mut socket = WebSocketClient::new(STOCKS_CLUSTER, None);
         let params = vec! ["T.MSFT"];
         socket.subscribe(&params);
+    }
+
+    #[test]
+    fn test_receive() {
+        let mut socket = WebSocketClient::new(STOCKS_CLUSTER, None);
+        let res = socket.receive();
+        assert_eq!(res.is_ok(), true);
+        let msg = res.unwrap();
+        assert_eq!(msg.is_text(), true);
+        let msg_str = msg.into_text().unwrap();
+        let messages: Vec<ConnectedMessage> = serde_json::from_str(&msg_str).unwrap();
+        let connected = messages.first().unwrap();
+        assert_eq!(connected.ev, "status");
+        assert_eq!(connected.status, "connected");
     }
 }

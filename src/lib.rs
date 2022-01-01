36dcd7179990b22c863c5b423df4e03c6402026a
Matt Conway <mattconway55@gmail.com>
2022-01-01 12:37:21 -0500

    add feature cfgs, clippy cleanup (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! Client library for [polygon.io](https://www.polygon.io).
+#[cfg(feature = "rest")]
 pub mod rest;
 pub mod types;
+#[cfg(feature = "websocket")]
 pub mod websocket;
 
 #[macro_use]
